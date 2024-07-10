ffed8df167711d731a29a02002620250f87745d8
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-07-10 13:43:24 +0200

    project init

---
+++ src/main/java/ch/zli/cospace/LbCospaceApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.cospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbCospaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbCospaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zli/cospace/LbCospaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.cospace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbCospaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
