1e3a01b1b4459c08f01addfbae020c34a5c82a63
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-07-11 14:26:32 +0200

    extended user to work with spring security

--- src/main/java/ch/zli/cospace/models/User.java
+++ src/main/java/ch/zli/cospace/models/User.java
@@ -3,7 +3,10 @@ package ch.zli.cospace.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -11,7 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
@@ -41,5 +44,36 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO: Add hirarchy of roles
+        return List.of(() -> role.name());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 }
