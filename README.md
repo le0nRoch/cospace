8f7014d7f8da0fb92fd6f8cb6fe388def4261cca
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-07-12 14:27:07 +0200

    added test data and described it

--- README.md
+++ README.md
@@ -2,4 +2,22 @@
 
 ## Test Data
 
-TODO: Describe the test data
+The test data is loaded on startup in
+the [CoworkingSpaceApplication](./src/main/java/ch/zli/cospace/LbCospaceApplication.java) class. \
+This happens only if the `spring.profiles.active` property is set to `dev`.
+
+### Users
+
+| Email                           | Password | First Name | Last Name | Role   |
+|---------------------------------|----------|------------|-----------|--------|
+| first.user.gets.admin@gmail.com | admin123 | First      | AdminUser | ADMIN  |
+| admin@gmail.com                 | admin123 | Admin      | Admin     | MEMBER |
+| deleteme@gmail.com              | deleteme | Delete     | Me        | MEMBER |
+| user@gmail.com                  | useruser | User       | User      | MEMBER |
+
+### Bookings
+
+| User Id | Date           | Timeslot | Status    | Description |
+|---------|----------------|----------|-----------|-------------|
+| 4       | {current date} | FULL_DAY | REQUESTED | {null}      |
+
--- src/main/java/ch/zli/cospace/LbCospaceApplication.java
+++ src/main/java/ch/zli/cospace/LbCospaceApplication.java
@@ -1,8 +1,9 @@
 package ch.zli.cospace;
 
+import ch.zli.cospace.dto.RegisterUserInput;
 import ch.zli.cospace.models.*;
 import ch.zli.cospace.repositories.BookingRepository;
-import ch.zli.cospace.repositories.UserRepository;
+import ch.zli.cospace.services.UserService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,15 +22,27 @@ public class LbCospaceApplication {
 
     @Profile("dev")
     @Bean
-    public ApplicationRunner runner(UserRepository userRepository, BookingRepository bookingRepository) {
+    public ApplicationRunner runner(UserService userService, BookingRepository bookingRepository) {
         return args -> {
-            // TODO: add needed test data
-            var user = new User(Role.ADMIN, "lro@gmx.ch", "gugus", "Leo", "Roch");
-            var booking = new Booking(user, LocalDate.now(), TimeSlot.FULL_DAY, BookingStatus.REQUESTED, null);
-            user.setBookings(List.of(booking));
+            var registerUserInput = new RegisterUserInput("first.user.gets.admin@gmail.com", "admin123", "First", "AdminUser");
+            var admin1Input = new RegisterUserInput("admin@gmail.com", "admin123", "Admin", "Admin");
+            var toDeleteUser = new RegisterUserInput("deleteme@gmail.com", "deleteme", "Delete", "Me");
+            var user2 = User.builder()
+                    .email("user@gmail.com")
+                    .password("useruser")
+                    .firstName("User")
+                    .lastName("User")
+                    .role(Role.MEMBER)
+                    .build();
+            var booking = new Booking(user2, LocalDate.now(), TimeSlot.FULL_DAY, BookingStatus.REQUESTED, null);
+            user2.setBookings(List.of(booking));
 
-            userRepository.saveAndFlush(user);
-            bookingRepository.saveAndFlush(booking);
+            userService.save(registerUserInput);
+            userService.save(admin1Input);
+            userService.save(toDeleteUser);
+            userService.save(user2);
+            bookingRepository.save(booking);
+
         };
     }
 }
